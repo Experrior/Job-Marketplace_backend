fe8caa440f6ab1e1de10b06c3dc93620ecdbd73f
Bohdan Kyryliuk <125163810+bodyakyryliuk@users.noreply.github.com>
2024-10-13 17:11:43 +0200

    Configure email consumer

--- services/notification-service/src/main/kotlin/org/jobsearch/notificationservice/rabbitmq/EmailConsumer.kt
+++ services/notification-service/src/main/kotlin/org/jobsearch/notificationservice/rabbitmq/EmailConsumer.kt
@@ -1,27 +1,29 @@
 package org.jobsearch.notificationservice.rabbitmq
 
+import org.jobsearch.notificationservice.requests.EmailRequest
+import org.jobsearch.notificationservice.requests.EmailType
+import org.jobsearch.notificationservice.services.EmailSendingService
+import org.slf4j.LoggerFactory
 import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.stereotype.Component
 
 
 @Component
-
-class EmailConsumer {
-    private val emailService: EmailService? = null
+class EmailConsumer (
+    private val emailSendingService: EmailSendingService
+){
+    private val logger = LoggerFactory.getLogger(EmailConsumer::class.java)
 
     @RabbitListener(queues = ["\${rabbitmq.queue.email}"])
     fun consumer(emailRequest: EmailRequest) {
         try {
-            log.info(("Sending message of " + emailRequest.getEmailType()).toString() + " type")
-            when (emailRequest.getEmailType()) {
-                VERIFICATION_EMAIL -> emailService.sendVerificationMail(emailRequest)
-                RESET_PASSWORD_EMAIL -> emailService.sendResetPasswordMail(emailRequest)
-                BUY_ORDER -> emailService.sendBuyOrderMail(emailRequest)
-                SELL_ORDER -> emailService.sendSellOrderMail(emailRequest)
-                else -> log.warn("Unhandled email type: {}", emailRequest.getEmailType())
+            logger.info(("Sending message of " + emailRequest.emailType) + " type")
+            when (emailRequest.emailType) {
+                EmailType.VERIFICATION_EMAIL -> emailSendingService.sendVerificationEmail(emailRequest)
+                EmailType.RESET_PASSWORD_EMAIL -> emailSendingService.sendResetPasswordEmail(emailRequest)
             }
         } catch (e: Exception) {
-            log.error("Error processing message", e)
+            logger.error("Error processing message", e)
         }
     }
 }
