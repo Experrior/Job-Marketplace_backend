998c1c85eec1a1d8b639fafc6f31bee4e5a19ed2
Bohdan Kyryliuk <125163810+bodyakyryliuk@users.noreply.github.com>
2024-10-13 15:23:04 +0200

    Create message queue service

---
+++ services/message-queue/src/main/kotlin/org/jobsearch/messagequeue/RabbitMQMessageProducer.kt
@@ -0,0 +1,19 @@
+package org.jobsearch.messagequeue
+
+import org.slf4j.LoggerFactory
+import org.springframework.amqp.core.AmqpTemplate
+import org.springframework.stereotype.Component
+
+
+@Component
+class RabbitMQMessageProducer(
+    private val amqpTemplate: AmqpTemplate
+){
+    private val logger = LoggerFactory.getLogger(RabbitMQMessageProducer::class.java)
+
+    fun publish(payload: Any?, exchange: String?, routingKey: String?) {
+        logger.info("publishing the message")
+        amqpTemplate.convertAndSend(exchange, routingKey, payload)
+        logger.info("published message")
+    }
+}
