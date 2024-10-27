451b153582dc18506265e7a697dc4aa1570acc62
Bohdan Kyryliuk <125163810+bodyakyryliuk@users.noreply.github.com>
2024-10-27 16:12:19 +0100

    Change requests to POST for verify email, reset password

--- services/user-service/src/main/kotlin/com/jobsearch/userservice/controllers/VerificationController.kt
+++ services/user-service/src/main/kotlin/com/jobsearch/userservice/controllers/VerificationController.kt
@@ -4,7 +4,6 @@ import com.jobsearch.userservice.services.auth.VerificationService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -14,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam
 class VerificationController(
     private val verificationService: VerificationService
 ) {
-    @GetMapping("/verify-email")
+    @PostMapping("/verify-email")
     fun verifyEmail(@RequestParam("token") token: String
     ): ResponseEntity<String> {
         verificationService.verifyByToken(token)
