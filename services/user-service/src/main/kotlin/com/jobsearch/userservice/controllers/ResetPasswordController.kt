58fb262093dd2ea675316984aeecc09f373384ed
Bohdan Kyryliuk <125163810+bodyakyryliuk@users.noreply.github.com>
2024-11-22 17:52:11 +0100

    Return ApiResponse in reset password and verification controllers.

--- services/user-service/src/main/kotlin/com/jobsearch/userservice/controllers/ResetPasswordController.kt
+++ services/user-service/src/main/kotlin/com/jobsearch/userservice/controllers/ResetPasswordController.kt
@@ -2,6 +2,7 @@ package com.jobsearch.userservice.controllers
 
 import com.jobsearch.userservice.requests.ResetPasswordRequest
 import com.jobsearch.userservice.requests.UpdatePasswordRequest
+import com.jobsearch.userservice.responses.ApiResponse
 import com.jobsearch.userservice.services.auth.tokens.ResetPasswordService
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
@@ -16,10 +17,11 @@ class ResetPasswordController(
     private val resetPasswordService: ResetPasswordService
 ) {
     @PostMapping("/resetPassword")
-    fun resetPassword(@RequestBody @Valid resetPasswordRequest: ResetPasswordRequest): ResponseEntity<String>{
+    fun resetPassword(@RequestBody @Valid resetPasswordRequest: ResetPasswordRequest): ResponseEntity<ApiResponse>{
         resetPasswordService.resetPassword(resetPasswordRequest.email)
+
         return ResponseEntity(
-            "Reset password link has been sent",
+            ApiResponse(message = "Reset password link has been sent"),
             HttpStatus.OK
         )
     }
@@ -27,10 +29,11 @@ class ResetPasswordController(
     @PostMapping("/updatePassword")
     fun updatePassword(
         @RequestParam("token") resetPasswordToken: String,
-        @RequestBody @Valid updatePasswordRequest: UpdatePasswordRequest): ResponseEntity<String>{
+        @RequestBody @Valid updatePasswordRequest: UpdatePasswordRequest): ResponseEntity<ApiResponse>{
         resetPasswordService.updatePassword(resetPasswordToken, updatePasswordRequest)
+
         return ResponseEntity(
-            "Password has been updated",
+            ApiResponse(message = "Password has been updated"),
             HttpStatus.OK
         )
     }
