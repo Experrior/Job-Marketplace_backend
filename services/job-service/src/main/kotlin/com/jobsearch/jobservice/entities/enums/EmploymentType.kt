30de84ad54c52a94b0e828a123491021d56455cc
Bohdan Kyryliuk <125163810+bodyakyryliuk@users.noreply.github.com>
2024-12-06 18:29:36 +0100

    Commit employment type enum

---
+++ services/job-service/src/main/kotlin/com/jobsearch/jobservice/entities/enums/EmploymentType.kt
@@ -0,0 +1,10 @@
+package com.jobsearch.jobservice.entities.enums
+
+enum class EmploymentType(val value: String) {
+    FULL_TIME("Full-time"),
+    PART_TIME("Part-time"),
+    CONTRACT("Contract"),
+    INTERNSHIP("Internship"),
+    TEMPORARY("Temporary"),
+    FREELANCE("Freelance")
+}
