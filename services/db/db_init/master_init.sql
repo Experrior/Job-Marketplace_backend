66af35ab013c484f3c2ada732d67dc756c1139a5
Mateusz Molenda <devexperrior@gmail.com>
2024-12-14 17:15:34 +0100

    fixes

--- services/db/db_init/master_init.sql
+++ services/db/db_init/master_init.sql
@@ -4,5 +4,3 @@ DROP USER IF EXISTS replicator;
 CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'replicator_password';
 
 SELECT pg_create_physical_replication_slot('replication_slot');
-
-CREATE DATABASE JobMarketDB;
